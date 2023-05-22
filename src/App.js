6b1b6d6ba4e7756110ce877462020a7f33f0f11f
Ellyboi <vitoesitemitope@gmail.com>
2023-05-22 20:59:11 +0100

    update linters

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import './styles/rockets.css';
-import Navbar from './components/Navbar';
-import Missions from './components/Missions';
-import Rockets from './components/Rockets';
-import MyProfile from './components/MyProfile';
-import Dragons from './components/Dragons';
 
 function App() {
   return (
@@ -14,10 +8,7 @@ function App() {
       <div>
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<MyProfile />} />
-          <Route exact path="missions" element={<Missions />} />
-          <Route exact path="rockets" element={<Rockets />} />
-          <Route exact path="dragons" element={<Dragons />} />
+         
         </Routes>
       </div>
     </>
